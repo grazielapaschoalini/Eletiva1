d80bdffe971bf36ad7f4c6e4ad60a37568d80953
Graziela Fernanda Paschoalini <graziela.paschoalini@gmail.com>
2024-08-24 11:29:46 -0300

    Atualiza Nomenclatura

--- Lista2/Exercicio1/exer1.php
+++ Lista2/Exercicio1/exer1.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio 1</title>
+    <title>Exercicio 01</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="container">
-    <h1>Exercicio 1</h1>
+    <h1>Exercicio 01</h1>
     <form action="exer1resp.php" method="POST">
     <div class="row">
             <div class="col">
--- Lista2/Exercicio1/exer1resp.php
+++ Lista2/Exercicio1/exer1resp.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Resposta do Exercício 1</title>
+    <title>Resposta do Exercício 01</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Resposta do Exercício 1</h1>
+    <h1>Exercício 01 - Some 02 Números</h1>
     <?php
         if($_SERVER["REQUEST_METHOD"]== 'POST'){
             //$valor1 = $_POST['valor1'];
