e0d1d80c4c1623f72234bcccaf3df084b6b3ef44
Graziela Fernanda Paschoalini <graziela.paschoalini@gmail.com>
2024-08-26 10:16:07 -0300

    Exercício, 16, 17, 18, 19, 20 e identação de todos

--- Lista2/Exercicio18/exer18.php
+++ Lista2/Exercicio18/exer18.php
@@ -4,21 +4,25 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Exercicio 08</title>
+  <title>Exercicio 18</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body class="container">
-  <h1>Exercício 08 - Área do Retângulo</h1>
-  <form action="exer8resp.php" method="POST">
+  <h1>Exercício 18 - Calcular Juros Compostos</h1>
+  <form action="exer18resp.php" method="POST">
     <div class="row">
       <div class="col">
-        <label for="valor1" class="form-label">Informe a largura (m): </label>
+        <label for="valor1" class="form-label">Informe o Capital (R$): </label>
         <input type="number" class="form-control" name="valor1" id="valor1">
       </div>
       <div class="col">
-        <label for="valor2" class="form-label">Informe a altura (m): </label>
+        <label for="valor2" class="form-label">Informe a Taxa de Juros a.a(%): </label>
         <input type="number" class="form-control" name="valor2" id="valor2">
+      </div>
+      <div class="col">
+        <label for="valor3" class="form-label">Informe o Período (ano): </label>
+        <input type="number" class="form-control" name="valor3" id="valor3">
       </div>
     </div>
     <div class="row">
@@ -29,5 +33,4 @@
   </form>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
-
 </html>
--- Lista2/Exercicio19/exer19.php
+++ Lista2/Exercicio19/exer19.php
@@ -4,21 +4,17 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Exercicio 08</title>
+  <title>Exercicio 19</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body class="container">
-  <h1>Exercício 08 - Área do Retângulo</h1>
-  <form action="exer8resp.php" method="POST">
+  <h1>Exercício 19 - Converter Valor</h1>
+  <form action="exer19resp.php" method="POST">
     <div class="row">
       <div class="col">
-        <label for="valor1" class="form-label">Informe a largura (m): </label>
+        <label for="valor1" class="form-label">Informe o Valor (Dias): </label>
         <input type="number" class="form-control" name="valor1" id="valor1">
-      </div>
-      <div class="col">
-        <label for="valor2" class="form-label">Informe a altura (m): </label>
-        <input type="number" class="form-control" name="valor2" id="valor2">
       </div>
     </div>
     <div class="row">
