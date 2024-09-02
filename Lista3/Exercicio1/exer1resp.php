b68155294e3f3c7d0d9bcd4b7d122062eab1241b
Graziela Fernanda Paschoalini <graziela.paschoalini@gmail.com>
2024-09-01 22:57:38 -0300

    endentação de cath e exercício 3

--- Lista3/Exercicio1/exer1resp.php
+++ Lista3/Exercicio1/exer1resp.php
@@ -37,7 +37,8 @@
       echo "<p>O menor valor é: $menorValor</p>";
       echo "<p>A posição do menor valor é a: " . ($posicaoMenor + 1) . "°</p>";
 
-    } catch (Exception $e) {
+    } catch (Exception $e) 
+    {
       echo "Erro! " . $e->getMessage();
     }
   }
