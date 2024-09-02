7df6dd1be5d5232b5482b8147be5b8870d95bbba
Graziela Fernanda Paschoalini <graziela.paschoalini@gmail.com>
2024-09-02 11:20:11 -0300

    espaçamento e exercicio da professora

--- Lista3/Exercicio1/exer1resp.php
+++ Lista3/Exercicio1/exer1resp.php
@@ -37,8 +37,9 @@
       echo "<p>O menor valor é: $menorValor</p>";
       echo "<p>A posição do menor valor é a: " . ($posicaoMenor + 1) . "°</p>";
 
-    } catch (Exception $e) {
-      echo "Erro! " .$e->getMessage();
+    } catch (Exception $e) 
+    {
+      echo "Erro! ".$e->getMessage();
     }
   }
   ?>
