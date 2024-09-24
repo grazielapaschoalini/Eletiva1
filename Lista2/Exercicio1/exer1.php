7c894a50530f4d2981892f9ee4d01737f7cab90d
Graziela Fernanda Paschoalini <graziela.paschoalini@gmail.com>
2024-09-24 20:21:58 -0300

    Aula 05 - Projeto

--- Lista2/Exercicio1/exer1.php
+++ Lista2/Exercicio1/exer1.php
@@ -8,7 +8,12 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body class="container">
+<body> <!--funções de inclusão Bootstrap Navbar - para chamar a função 2 funções e duas variações (include)  abre mesmo a função dando erro (require) se der erro na função ele trava e nao abre-->
+<!--include once ou require once se vc chamar a mesma função 2 x ela nao repete -->
+  <?php 
+    require_once("../nav.php"); // ../ chama a pasta anterior
+  ?>
+  <main class="container">
   <h1>Exercício 01 - Some 02 Números</h1>
   <form action="exer1resp.php" method="POST">
     <div class="row">
@@ -27,6 +32,7 @@
       </div>
     </div>
   </form>
+  </main>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
