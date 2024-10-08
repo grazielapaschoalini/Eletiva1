2deb9728055e26b1010d7fa2c16a45df82bc5fc2
Graziela Fernanda Paschoalini <graziela.paschoalini@gmail.com>
2024-10-08 00:32:19 -0300

    Novo_cliente

---
+++ Projeto Sistema de Gerenciamento de Transportadora/paginas/novo_cliente.php
@@ -0,0 +1,24 @@
+<?php 
+    require_once 'cabecalho.php'; 
+    require_once 'navbar.php'; 
+?>
+
+<div class="container mt-5">
+    <h2>Novo Cliente</h2>
+    <form action="novo_cliente.php" method="post">
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="nome" name="nome" required>
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="mb-3">
+            <label for="senha" class="form-label">Senha</label>
+            <input type="password" name="senha" id="senha" class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </form>
+
+<?php require_once 'rodape.php'; ?>
