473119c77a75926c4c4fb4cbcd37e49b1c6f1f6d
Graziela Fernanda Paschoalini <graziela.paschoalini@gmail.com>
2024-10-08 00:37:03 -0300

    Editar_cliente

---
+++ Projeto Sistema de Gerenciamento de Transportadora/paginas/editar_cliente.php
@@ -0,0 +1,24 @@
+<?php 
+    require_once 'cabecalho.php'; 
+    require_once 'navbar.php';
+?>
+
+<div class="container mt-5">
+    <h2>Editar Cliente</h2>
+    <form action="editar_cliente.php?id=<?php echo $cliente['id']; ?>" method="post">
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="nome" name="nome" value="<?php echo $cliente['nome']; ?>" required>
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" value="<?php echo $cliente['email']; ?>" required>
+        </div>
+        <div class="mb-3">
+            <label for="senha" class="form-label">Nova Senha</label>
+            <input type="password" class="form-control" id="senha" name="senha" value="<?php echo $cliente['senha']; ?>" required>  >
+        </div>
+        <button type="submit" class="btn btn-primary">Atualizar</button>
+    </form>
+
+<?php require_once 'rodape.php'; ?>
