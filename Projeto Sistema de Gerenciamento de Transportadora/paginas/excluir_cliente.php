c5803b950314d828a6fc2eab0ef4d33916472805
Graziela Fernanda Paschoalini <graziela.paschoalini@gmail.com>
2024-11-18 12:13:41 -0300

    Paginas - Excluir clientes

--- Projeto Sistema de Gerenciamento de Transportadora/paginas/excluir_cliente.php
+++ Projeto Sistema de Gerenciamento de Transportadora/paginas/excluir_cliente.php
@@ -1,20 +1,9 @@
-<?php 
-    require_once 'cabecalho.php'; 
-    require_once 'navbar.php';
-?>
+<?php
+require_once '../funcoes/clientes.php';
 
-<div class="container mt-5">
-    <h2>Excluir Cliente</h2>
-    <p>Tem certeza de que deseja excluir o cliente abaixo?</p>
-    <ul>
-        <li><strong>Nome:</strong> </li>
-        <li><strong>Email:</strong> </li>
-        <li><strong>Nível:</strong> </li>
-    </ul>
-    <form method="post">
-        <button type="submit" name="confirmar" class="btn btn-danger">Excluir</button>
-        <a href="clientes.php" class="btn btn-secondary">Cancelar</a>
-    </form>
-</div>
-
-<?php require_once 'rodape.php'; ?>
+$id = $_GET['id'] ?? null;
+if ($id && excluirCliente((int)$id)) {
+    header("Location: clientes.php");
+} else {
+    echo "<p class='text-danger'>Erro ao excluir cliente!</p>";
+}
