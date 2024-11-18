08d310964c2d1328e7ac7fa785f205822ac2a366
Graziela Fernanda Paschoalini <graziela.paschoalini@gmail.com>
2024-11-18 11:57:42 -0300

    Paginas - Excluir Usuario

---
+++ Projeto Sistema de Gerenciamento de Transportadora/paginas/excluir_usuario.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../funcoes/usuarios.php';
+
+$id = $_GET['id'] ?? null;
+if ($id && excluirUsuario((int)$id)) {
+    header("Location: usuarios.php");
+} else {
+    echo "<p class='text-danger'>Erro ao excluir usuário!</p>";
+}
