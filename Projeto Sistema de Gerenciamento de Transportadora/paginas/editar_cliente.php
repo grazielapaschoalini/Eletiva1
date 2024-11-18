40b8c313be044d988e2a4423c336f30f6753a2ac
Graziela Fernanda Paschoalini <graziela.paschoalini@gmail.com>
2024-11-18 12:13:01 -0300

    Paginas - Editar clientes

--- Projeto Sistema de Gerenciamento de Transportadora/paginas/editar_cliente.php
+++ Projeto Sistema de Gerenciamento de Transportadora/paginas/editar_cliente.php
@@ -1,25 +1,49 @@
-<?php 
-    require_once 'cabecalho.php'; 
-    require_once 'navbar.php';
+<?php
+require_once 'cabecalho.php';
+require_once 'navbar.php';
+require_once '../funcoes/clientes.php';
+
+$id = $_GET['id'] ?? null;
+if (!$id) {
+    header("Location: clientes.php");
+    exit;
+}
+
+$cliente = retornaClientePorId((int)$id);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+
+    if (editarCliente((int)$id, $nome, $cpf, $email, $telefone)) {
+        header("Location: clientes.php");
+        exit;
+    } else {
+        echo "<p class='text-danger'>Erro ao editar cliente!</p>";
+    }
+}
 ?>
 
 <div class="container mt-5">
     <h2>Editar Cliente</h2>
-    <form action="editar_cliente.php?id=<?php echo $cliente['id']; ?>" method="post">
+
+    <form method="post">
         <div class="mb-3">
             <label for="nome" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="nome" name="nome" value="<?php echo $cliente['nome']; ?>" required>
+            <input type="text" name="nome" id="nome" class="form-control" value="<?= $cliente['nome']; ?>" required>
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" value="<?php echo $cliente['email']; ?>" required>
+            <input type="email" name="email" id="email" class="form-control" value="<?= $cliente['email']; ?>" required>
         </div>
         <div class="mb-3">
-            <label for="senha" class="form-label">Nova Senha</label>
-            <input type="password" class="form-control" id="senha" name="senha" value="<?php echo $cliente['senha']; ?>" required>  >
+            <label for="telefone" class="form-label">Telefone</label>
+            <input type="text" name="telefone" id="telefone" class="form-control" value="<?= $cliente['telefone']; ?>" required>
         </div>
-        <button type="submit" class="btn btn-primary">Atualizar</button>
+        <button type="submit" class="btn btn-primary">Salvar Alterações</button>
     </form>
 </div>
 
 <?php require_once 'rodape.php'; ?>
+
